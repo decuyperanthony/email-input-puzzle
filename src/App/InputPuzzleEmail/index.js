897306b7a1838345bc95de76d8b36509a83dd2ed
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-12-18 08:55:52 +0100

    update comments on inputPuzzleEmail Components

--- src/App/InputPuzzleEmail/index.js
+++ src/App/InputPuzzleEmail/index.js
@@ -11,14 +11,14 @@ const InputPuzzleEmail = () => {
     //* concernant le bonus, la solution est dans le code avec
     //* const inputEl = useRef(null);
     //* inputEl.current.focus();
-    //* Mais elle ne se voit pas car j'ai ajouté une class success
-    //* J'espère ne pas faire un hors sujet :(
+    //* Mais cela se voit que lorsque l'input est vide
+    //* et que l'on clique sur une des suggestions
+    //* Car j'ai ajouté une class success lorsque le mail a un format correct
     //* ----------------------------------
 
     const regexEmail = /^[\w.=-]+@[\w.-]+\.[\w]{2,3}$/i;
     const regexAt = /@/;
 
-    //! tableau à refaire plus proprement
     const initialProviderSuggestion =
     [
         providers[0],
@@ -29,26 +29,27 @@ const InputPuzzleEmail = () => {
     const [inputValue, setInputValue] = useState('');
     const [successClass, setSucessClass] = useState(false);
     const [providerSuggestion, setProviderSuggestion] = useState(initialProviderSuggestion);
+    //! cela me permettra de donner le focus à mon input
     const inputEl = useRef(null);
 
     const handleChangeInputValue = (e) => {
         setSucessClass(false);
         setInputValue(e.target.value);
-        //! pour filtrer mon tableau je recupere la valeur qu'il y avant le point. ==> p.substr(0, p.indexOf('.'))
-        //! puis celle apres le point jusqu'au arobase ==> .indexOf(e.target.value.substring(e.target.value.indexOf('@') + 1))
-        //! je filtre en comparant avec le nom du provider
+        //! pour filtrer mon tableau je recupère la valeur qu'il y a avant le point "." ==> p.substr(0, p.indexOf('.'))
+        //! puis celle apres le "arobase" jusqu'au "." ==> .indexOf(e.target.value.substring(e.target.value.indexOf('@') + 1))
+        //! je filtre en le comparant avec le nom du provider
         const filteredProviders = providers.filter((p) => p.substr(0, p.indexOf('.')).indexOf(e.target.value.substring(e.target.value.indexOf('@') + 1)) !== -1);
         e.target.value === '' && setProviderSuggestion(initialProviderSuggestion);
-        //!
-        console.log('e.target.value.substr(e.target.value.indexOf(@))', e.target.value.substr(e.target.value.indexOf('@')))
+        //! Dès que la saisi du user contient un "@" => je remplace mon state de suggestion par le tableau filtré des fournisseurs
         e.target.value.substr(e.target.value.indexOf('@')).match(regexAt) && setProviderSuggestion([...filteredProviders]);
         e.target.value.match(regexEmail) && setProviderSuggestion([]);
     };
 
     const handleSetProviders = (prov) => {
+        //! je donne le focus à mon input lorsque le user clique sur une des suggestions
         inputEl.current.focus();
         setProviderSuggestion([]);
-        //! si l input value comporte un arobase j'enleve tout ce qu'il y apres le arobose
+        //! si l input value comporte un "@" j'enleve tout ce qu'il y apres le "@"
         inputValue.match(regexAt) ?
             setInputValue(inputValue.substring(0, inputValue.indexOf('@')) + '@' + prov) :
             //! sinon je prend la valeur de l'input que je concataine avec le reste
@@ -57,6 +58,7 @@ const InputPuzzleEmail = () => {
 
     useEffect(() => {
         //! si le regex email match avec la value de l'input
+        //! j'ajoute la class email
         const regexEmail = /^[\w.=-]+@[\w.-]+\.[\w]{2,3}$/i;
         inputValue.match(regexEmail) && setSucessClass(true);
     }, [inputValue])
